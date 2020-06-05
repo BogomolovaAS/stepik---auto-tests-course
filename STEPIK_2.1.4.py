671f1e23bb4ac38ae86379fe39c4e84ca465df17
BogomolovaAS <zf2070406@yandex.ru>
2020-06-05 09:44:31 +0700

    Добавление комментариев в коде

--- STEPIK_2.1.4.py
+++ STEPIK_2.1.4.py
@@ -7,25 +7,31 @@ def calc(x):
   return str(math.log(abs(12*math.sin(int(x)))))
 
 try:
-
+    
+    # Перход на нужную вкладку
     link = " http://SunInJuly.github.io/execute_script.html"
     browser = webdriver.Chrome()
     browser.get(link)
 
+    # Поиск числа с которым необходимо провести расчеты
     x_element = browser.find_element_by_id("input_value")
     x = x_element.text
     y = calc(x)
 
+    # Ввод полученного ответа в поле
     input = browser.find_element_by_id("answer")
     browser.execute_script("return arguments[0].scrollIntoView(true);", input)
     input.send_keys(y)
 
+    # Выбор чек-бокса
     option1 = browser.find_element_by_id("robotCheckbox")
     option1.click()
-
+    
+    # Выбор радиобатона
     option2 = browser.find_element_by_id("robotsRule")
     option2.click()
 
+    # Нажатие на кнопку отправки ответа
     button = browser.find_element_by_css_selector("button.btn")
     button.click()
 
